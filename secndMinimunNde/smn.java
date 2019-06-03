a01deb507c0f6ac7616cfb06b4fe787561304359
llwsykll <574833620@qq.com>
2019-06-03 19:45:09 +0800

    6.3

---
+++ secndMinimunNde/smn.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int min1;
+    long ans = Long.MAX_VALUE;
+
+    public void dfs(TreeNode root) {
+        if (root != null) {
+            if (min1 < root.val && root.val < ans) {
+                ans = root.val;
+            } else if (min1 == root.val) {
+                dfs(root.left);
+                dfs(root.right);
+            }
+        }
+    }
+    public int findSecondMinimumValue(TreeNode root) {
+        min1 = root.val;
+        dfs(root);
+        return ans < Long.MAX_VALUE ? (int) ans : -1;
+    }
+}
---
+++ secndMinimunNde/smn.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        self.ans = float('inf')
+        min1 = root.val
+
+        def dfs(node):
+            if node:
+                if min1 < node.val < self.ans:
+                    self.ans = node.val
+                elif node.val == min1:
+                    dfs(node.left)
+                    dfs(node.right)
+
+        dfs(root)
+        return self.ans if self.ans < float('inf') else -1
